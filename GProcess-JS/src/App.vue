675f37f3506d4cb1a1bedcfb73254a390fb3345f
Nicsi <nicolas.siarri@edu.esiee.fr>
2018-10-18 11:04:31 +0200

    vue authentification JS

--- GProcess-JS/src/App.vue
+++ GProcess-JS/src/App.vue
@@ -1,21 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div id="left-pane">
+      <img src="./assets/logo.png" width="50%"> 
+      <h2>GProcess - Connexion</h2>
+      <p> Nom de compte : </p>
+      <input type="text">
+      <p> Mot de passe </p>
+      <input type="password">
+      <p><button > Me connecter </button> </p>
+    </div>
+    <div id="right-pane">
+      <img src="./assets/splash_screen.jpg" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -35,26 +31,38 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+   width:100%;
+   height: 100%;
+   margin: 0;
+   overflow: hidden;
+   padding: 0;
+}
+
+#left-pane
+{
+  width: 25%;
+  height: 100%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  padding-top:20px;
+  overflow: hidden;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
+  
 }
 
-h1, h2 {
+#right-pane
+{
+  width: 75%;
+  height: 100%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  right:0;
+  overflow: hidden;
+}
+h1, h2, p {
   font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
