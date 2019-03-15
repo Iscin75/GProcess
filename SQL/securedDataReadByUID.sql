e0ee15a864280cd1232af65cd315fea374c2a961
Nicsi <nicolas.siarri@edu.esiee.fr>
2019-03-15 10:20:17 +0100

    - securedDataReadByUID.sql update

--- SQL/securedDataReadByUID.sql
+++ SQL/securedDataReadByUID.sql
@@ -2,37 +2,49 @@ CREATE OR REPLACE FUNCTION securedDataReadByUID(user_id VARCHAR, curr_data_id VA
 RETURNS jsonb AS $$
 
 DECLARE 
-	s_g VARCHAR[10];
-	s_g_users VARCHAR[10];
-	cur_user VARCHAR;
-	cur_sg VARCHAR;
-	is_ok BOOLEAN = FALSE;
-	to_return jsonb;
-
+	
+ sg_groups VARCHAR[10];
+ cur_sg VARCHAR;
+ cur_members VARCHAR[10];
+ cur_member VARCHAR;
+ cur_priority INT;
+ priorities INT[10];
+ to_return jsonb;
+ 
 BEGIN
 	
-	SELECT security_groups INTO s_g FROM data WHERE data_id = curr_data_id ;
-	FOREACH cur_sg in ARRAY s_g
+	SELECT security_groups INTO sg_groups FROM RealmObject WHERE ro_id = curr_data_id;
+	
+	FOREACH cur_sg in ARRAY sg_groups
 	LOOP
-		SELECT members INTO s_g_users FROM SecurityGroup WHERE sg_id = cur_sg;
-		FOREACH cur_user in ARRAY s_g_users
+		SELECT members INTO cur_members FROM SecurityGroup WHERE sg_id = cur_sg;
+		FOREACH cur_member in ARRAY cur_members
 		LOOP
-			IF user_id = cur_user THEN
-				is_ok = TRUE;
-				EXIT;
-			END IF;
+			IF cur_member = user_id THEN
+					SELECT priority INTO cur_priority FROM Permissions WHERE p_id = 
+					(SELECT read_p FROM SecurityGroup WHERE sg_id = cur_sg);
+					SELECT array_append(priorities, cur_priority);
+				END IF;	
 		END LOOP;
-		IF is_ok = TRUE THEN
-			EXIT;
-		END IF;	
+		
 	END LOOP;
 	
-	IF is_ok THEN
-		SELECT
-		datas INTO to_return FROM data WHERE data_id = curr_data_id ;
-		return to_return;
-	END IF;
+	cur_priority = array_lower(priorities);
 	
+	CASE cur_priority
+		WHEN 1 OR 3 THEN
+			SELECT datas INTO to_return FROM data WHERE data_id = curr_data_id ;
+			return to_return;
+		WHEN 2 THEN
+			RAISE EXCEPTION 'Forbidden';
+		WHEN 4 THEN
+			RAISE EXCEPTION 'None';
+		WHEN 5 THEN
+			RAISE EXCEPTION 'Undefined';
+		WHEN 6 THEN
+			RAISE EXCEPTION 'Unrelated';
+	
+	END CASE;
 END; $$
 
 LANGUAGE 'plpgsql';
