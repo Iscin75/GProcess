cb5587b37575f49804e8d90cdce10dc8a27e6138
PaulMoua <paulnova@hotmail.fr>
2019-03-30 23:40:16 +0100

    Testé 30/3 OK

--- SQL/securedSGRead.sql
+++ SQL/securedSGRead.sql
@@ -18,26 +18,15 @@ DECLARE
 
 BEGIN
 	
-	cur_priority  = getReadPriority(user_id, psg_id);
 	
-	CASE cur_priority
-		WHEN 1 OR 3 THEN
-	
-			RETURN QUERY SELECT *
+	IF check_priority(user_id, psg_id) = TRUE THEN 
+		RETURN QUERY SELECT *
 			FROM
 			SecurityGroup
 			WHERE
 			sg_id = psg_id;
-		WHEN 2 THEN
-			RAISE EXCEPTION 'Forbidden';
-		WHEN 4 THEN
-			RAISE EXCEPTION 'None';
-		WHEN 5 THEN
-			RAISE EXCEPTION 'Undefined';
-		WHEN 6 THEN
-			RAISE EXCEPTION 'Unrelated';
-	
-	END CASE;
+	END IF;
+
 	
 	
 END; $$
