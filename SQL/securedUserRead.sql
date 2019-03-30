f2da23ef7952c48078c31862380029bb97657fd9
PaulMoua <paulnova@hotmail.fr>
2019-03-30 23:50:57 +0100

    Testé 30/3 OK

--- SQL/securedUserRead.sql
+++ SQL/securedUserRead.sql
@@ -15,27 +15,14 @@ DECLARE
 
 BEGIN
 
-	
-	cur_priority = getReadPriority(user_id , dest_user_id) ;
-	
-	CASE cur_priority
-		WHEN 1 OR 3 THEN
-	
-			RETURN QUERY SELECT *
+	IF check_priority(user_id , dest_user_id) = TRUE THEN 
+					RETURN QUERY SELECT *
 			FROM
 			Users
 			WHERE
 			u_id = dest_user_id;
-		WHEN 2 THEN
-			RAISE EXCEPTION 'Forbidden';
-		WHEN 4 THEN
-			RAISE EXCEPTION 'None';
-		WHEN 5 THEN
-			RAISE EXCEPTION 'Undefined';
-		WHEN 6 THEN
-			RAISE EXCEPTION 'Unrelated';
-	
-	END CASE;
+	END IF;
+
 	
 	
 END; $$
