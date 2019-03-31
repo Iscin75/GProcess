49b4730d3a373fd30c207349962dcf2a9e87e201
PaulMoua <paulnova@hotmail.fr>
2019-03-31 01:47:46 +0100

    Testé 31/3 ok

--- SQL/securedUserUpdate.sql
+++ SQL/securedUserUpdate.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION securedUserCredentialsUpdate(user_id VARCHAR, dest_user_id VARCHAR, colName VARCHAR, val VARCHAR)
-RETURNS VOID AS $$
+RETURNS INT AS $$
 
 DECLARE 
 
@@ -9,12 +9,16 @@ BEGIN
 
 cur_priority = getUpdatePriority(user_id , dest_user_id ) ;
 
-CASE cur_priority
-		WHEN 1 OR 3 THEN
-	
-			UPDATE Users SET colName = val WHERE u_id = dest_user_id;
+	CASE cur_priority
+		WHEN 1 THEN
+			EXECUTE format('UPDATE users SET %1$I=%2$L WHERE u_id=%3$L',colname, val,dest_user_id  );
+			
+			 /*UPDATE securityGroup SET colname = autorization WHERE sg_id = psg_id;*/
 		WHEN 2 THEN
 			RAISE EXCEPTION 'Forbidden';
+		WHEN  3 THEN
+				EXECUTE format('UPDATE users SET %1$I=%2$L WHERE u_id=%3$L',colname, val,dest_user_id  );
+			
 		WHEN 4 THEN
 			RAISE EXCEPTION 'None';
 		WHEN 5 THEN
@@ -24,7 +28,7 @@ CASE cur_priority
 	
 	END CASE;
 	
+	RETURN cur_priority;
 	
 END; $$
 LANGUAGE 'plpgsql';
-
