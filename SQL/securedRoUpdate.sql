6de87ac6e6297d5350bfc969ba2783c0d7f88330
Nicsi <nicolas.siarri@edu.esiee.fr>
2019-04-07 14:03:50 +0200

    secured JSON/array update

--- SQL/securedRoUpdate.sql
+++ SQL/securedRoUpdate.sql
@@ -21,3 +21,51 @@ BEGIN
 	
 END; $$
 LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION securedRODataUpdate(user_id VARCHAR, ro_id VARCHAR, val jsonb)
+RETURNS INT AS $$
+
+DECLARE 
+
+	cur_priority INT;
+
+BEGIN
+
+	cur_priority = getUpdatePriority(user_id , ro_id ) ;
+
+
+	IF check_update_priority(user_id , ro_id) = TRUE THEN 
+			EXECUTE format('UPDATE RealmObject SET data_core=%1$L WHERE ro_id=%2$L', val,ro_id  );
+	END IF;
+
+
+	
+	
+	RETURN cur_priority;
+	
+END; $$
+LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION securedROSGUpdate(user_id VARCHAR, ro_id VARCHAR, val text[])
+RETURNS INT AS $$
+
+DECLARE 
+
+	cur_priority INT;
+
+BEGIN
+
+	cur_priority = getUpdatePriority(user_id , ro_id ) ;
+
+
+	IF check_update_priority(user_id , ro_id) = TRUE THEN 
+			EXECUTE format('UPDATE RealmObject SET security_groups=%1$L WHERE ro_id=%2$L', val,ro_id  );
+	END IF;
+
+
+	
+	
+	RETURN cur_priority;
+	
+END; $$
+LANGUAGE 'plpgsql';
